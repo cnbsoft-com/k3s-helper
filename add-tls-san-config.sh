5cd77ce895217e86d1f7e3e95cc7286ea8cec561
dino <tolaputa76@gmail.com>
2025-09-24 23:54:49 +0900

    Add tls-san add script

--- add-tls-san-config.sh
+++ add-tls-san-config.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 
-read -p "cluster name : " cluster_name
-temp_file_name="${cluster_name}_config.txt"
+set cluster_name, domain, temp_file_name, isApply
 
-echo "tls-san:" > $temp_file_name
-echo "  - cnbsoft.com" >> $temp_file_name
+readClusterName() {
+  read -p "cluster name : " cluster_name
+  temp_file_name="${cluster_name}_config.txt"
+}
 
-for i in $(multipass exec ${cluster_name}-master -- ip a | grep -v grep | grep enp | grep inet | awk '{print$2}' | awk -F/ '{print$1}'); do
-  config="${config}  - ${i}\r\n"
-  cat<<EOF >> $temp_file_name
+readDomainName(){
+  read -p "domain name[not mandatory] : " domain
+}
+
+confirmTypeInfo(){
+  echo "Cluster Name is ${cluster_name}"
+  if [[ ! -z $domain ]]; then
+    echo "Domain is ${domain}"
+  fi
+
+  read -p "Apply config?[Y/n]" isApply
+}
+
+writeTempTlsSanConfig(){
+  echo "tls-san:" > $temp_file_name
+  if [[ ! -z $domain ]]; then
+    echo "  - $domain" >> $temp_file_name
+  fi
+
+  for i in $(multipass exec ${cluster_name}-master -- ip a | grep -v grep | grep enp | grep inet | awk '{print$2}' | awk -F/ '{print$1}'); do
+    config="${config}  - ${i}\r\n"
+    cat<<EOF >> $temp_file_name
   - ${i}
 EOF
-done
+  done
+}
 
-cat $temp_file_name | multipass exec ${cluster_name}-master -- sudo tee /etc/rancher/k3s/config.yaml
-rm -rf $temp_file_name
-multipass exec ${cluster_name}-master -- sudo systemctl restart k3s
+applyTlsSanConfig(){
+  cat $temp_file_name | multipass exec ${cluster_name}-master -- sudo tee /etc/rancher/k3s/config.yaml
+  rm -rf $temp_file_name
+  multipass exec ${cluster_name}-master -- sudo systemctl restart k3s
+}
+
+
+readClusterName
+readDomainName
+confirmTypeInfo
+
+if [[ -z $isApply || "Y" == $isApply ]]; then
+  writeTempTlsSanConfig
+  applyTlsSanConfig
+else
+  exit 0
+fi
+
